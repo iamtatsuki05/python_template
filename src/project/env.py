2aebfd91317fe84aad9f26924f7a0789ef0258c6
iamtatsuki05 <tatsukio0522@gmail.com>
2024-05-20 01:57:05 +0900

    add version test

--- src/project/env.py
+++ src/project/env.py
@@ -1,3 +1,5 @@
 from pathlib import Path
 
 PACKAGE_DIR = Path(__file__).parents[2]
+
+VERSION = '0.1.0'
---
+++ tests/project/test_env.py
@@ -0,0 +1,5 @@
+from project.env import VERSION
+
+
+def test_version():
+    assert VERSION == '0.1.0'
